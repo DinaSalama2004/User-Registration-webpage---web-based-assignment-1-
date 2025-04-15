c923e236857b3616928de59bcdc2d449d31e4534
Mariam Assem <130317896+mar-yam11@users.noreply.github.com>
2025-04-15 19:32:07 +0200

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "mariam004";
+$password = "";
 $database = "mydb";  //name of your data base you create it
 
 // Create a connection
@@ -39,17 +39,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["fileToUpload"])) {
             $sql = "INSERT INTO user_images (image_name, upload_date) VALUES (?, NOW())";
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("s", $uniqueFileName);
-
-            // Option 2: Store both image file and name in the database
-            // Uncomment the code below to store the image file in the database
-
-            $imageData = file_get_contents($targetFilePath);
-            $sql = "INSERT INTO user_images (image_name, image_data, upload_date) VALUES (?, ?, NOW())";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("ss", $uniqueFileName, $imageData);
-
-            // If you store the image in the database, you might want to delete the file from the server
-            unlink($targetFilePath);
 
             if ($stmt->execute()) {
                 $status = "The file " . $uniqueFileName . " has been uploaded successfully.";
